d424563a25923168154d24014350993915b0e0fc
Sławek Borowiec <przodownik@tlen.pl>
2016-09-14 11:19:08 +0200

    spring data rest added

--- src/main/java/pl/java/scalatech/repository/PersonResporitory.java
+++ src/main/java/pl/java/scalatech/repository/PersonResporitory.java
@@ -1,9 +1,10 @@
 package pl.java.scalatech.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import pl.java.scalatech.domain.Person;
-
+@RepositoryRestResource(collectionResourceRel="person",path="osoby")
 public interface PersonResporitory extends JpaRepository<Person, Long>{
 
 }
